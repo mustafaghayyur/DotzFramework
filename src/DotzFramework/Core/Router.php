f70d89c496237a1347915c102634a90899090e81
Mustafa Ghayyur <mghayyur@gmail.com>
2020-02-10 12:38:15 -0500

    fixed indenting in router file

--- src/DotzFramework/Core/Router.php
+++ src/DotzFramework/Core/Router.php
@@ -9,15 +9,15 @@ class Router {
 	public $configs;
 
 	/**
-	 * Sets the configs for this router.
-	 */
+	* Sets the configs for this router.
+	*/
 	public function __construct($configs){
 		$this->configs = $configs;
 	}
 
 	/**
-	 * Does the routing.
-	 */
+	* Does the routing.
+	*/
 	public function do(){
 
 		if(!$this->areConfigsDefined()){
@@ -32,11 +32,11 @@ class Router {
 
 		// If there are no uri elements, load the default page.
 		if(empty($uri[0])){
-			
+
 			$cObj = $this->instantiateClass(
-				$this->configs->router->default->controller, 
-				$this->configs->router->default->method
-			);
+						$this->configs->router->default->controller, 
+						$this->configs->router->default->method
+					);
 
 			if(!$cObj){
 				throw new Exception('Could not load Default page.');
@@ -45,17 +45,17 @@ class Router {
 			$controller = [ $cObj, $this->configs->router->default->method, [] ];
 
 		}
-        
+
 		// Is it a custom URL defined in the router configs?
 		if(empty($controller)
 			&& isset($this->configs->router->customRules->{$uri[0]})){
-			$controller = $this->tryCustomRulesCall($uri);
+				$controller = $this->tryCustomRulesCall($uri);
 		}
 
 		// Is it a REST resource?
 		if(empty($controller)
 			&& isset($this->configs->router->restResources->{$uri[0]})){
-			$controller = $this->tryRestResourceCall($uri);
+				$controller = $this->tryRestResourceCall($uri);
 		}
 
 		// If no luck yet; try using the first two URI elelemnts as a
@@ -66,15 +66,15 @@ class Router {
 
 		// If a controller is found call it; or call the Not Found Page
 		if($controller){ 
-			
+
 			call_user_func_array([$controller[0], $controller[1]], $controller[2]);			
-		
+
 		}else{
 
 			$cObj = $this->instantiateClass(
-				$this->configs->router->notFound->controller, 
-				$this->configs->router->notFound->method
-			);
+						$this->configs->router->notFound->controller, 
+						$this->configs->router->notFound->method
+					);
 
 			if(!$cObj){
 				throw new Exception('Could not load Not Found page.');
@@ -87,19 +87,19 @@ class Router {
 	}
 
 	/**
-	 * Tries to fetch the ControllerClass->method combination for the 
-	 * RESTful resource.
-	 */
+	* Tries to fetch the ControllerClass->method combination for the 
+	* RESTful resource.
+	*/
 	public function tryRestResourceCall($uri){
 
 		$c = $this->configs->router->restResources->{$uri[0]};
 		$m = SymfonyRequest::get()->object->getMethod();
-		
+
 		$httpMethodIssues = strpbrk($m, "#$%^&*()+=[]';,./{}|:<>?~");
 		$m = (!$httpMethodIssues) ? strtolower($m) . 'Resource' : null;
 
 		$cObj = $this->instantiateClass($c, $m);
-					
+
 		$args = []; 
 
 		foreach ($uri as $key => $value) {
@@ -107,22 +107,22 @@ class Router {
 				$args[] = $value;
 			}
 		}
-		
+
 		return ($cObj) ? [$cObj, $m, $args] : null;
-	
+
 	}
 
 	/**
-	 * Tries to fetch the ControllerClass->method combination for the
-	 * custom URI provided by the user. 
-	 */
+	* Tries to fetch the ControllerClass->method combination for the
+	* custom URI provided by the user. 
+	*/
 	public function tryCustomRulesCall($uri){
-		
+
 		$customRule = $this->configs->router->customRules->{$uri[0]};
 
 		$cObj = $this->instantiateClass($customRule->controller, $customRule->method);
 
-        $args = []; 
+		$args = []; 
 
 		foreach ($uri as $key => $value) {
 			if($key > 0){
@@ -133,15 +133,15 @@ class Router {
 		return 	($cObj) ? [$cObj, $customRule->method, $args] : null;
 	}
 
-    /**
-     * Tries to fetch the ConrtollerClass->method combination based on
-     * the first two URI elements determined by this::getUri() method.
-     */
+	/**
+	* Tries to fetch the ConrtollerClass->method combination based on
+	* the first two URI elements determined by this::getUri() method.
+	*/
 	public function trySimpleControllerCall($uri){
-		
+
 		$class = ucfirst($uri[0]) .'Controller';
 		$cObj = $this->instantiateClass($class, $uri[1]);
-		
+
 		$args = []; 
 
 		foreach ($uri as $key => $value) {
@@ -156,104 +156,105 @@ class Router {
 	}
 
 	/**
-	 * Tries to locate the given class and method and instansiate the class object.
-	 */
+	* Tries to locate the given class and method and instansiate the class object.
+	*/
 	private function instantiateClass($class, $method){
 
-		$file = '/'. trim($this->configs->app->appSystemPath, '/') .'/'. 
+		$file = '/'. trim($this->configs->app->appSystemPath, '/') .'/'.
 				trim($this->configs->router->controllersDirectory, '/') .'/'.
 				$class.'.php';
-		
+
 		if(!@include($file)) {
 			return false;
 		}
 
 		// Check to see if controller exists
 		if(class_exists($class)){
-			
+
 			$cObj = new $class();
 			$method = (empty($method)) ? 'index' : $method;
 
 			// Check if method is callable
 			if(method_exists($cObj, $method)){
-                return $cObj;
+				return $cObj;
 			}
 		}
 
 		return false;
 	}
-	
-	/**
-	 * Checks to see if required configurations are defined.
-	 */
-	public function areConfigsDefined(){
-	    
-	    if(isset($this->configs->app->appURL)
-	            && isset($this->configs->router->controllersDirectory)
-	           	&& isset($this->configs->router->default) 
-	           	&& is_object($this->configs->router->default)
-	           	&& isset($this->configs->router->notFound) 
-	           	&& is_object($this->configs->router->notFound)){
-	        
-	        return true;
-	    }
 
-	    return false;
+	/**
+	* Checks to see if required configurations are defined.
+	*/
+	public function areConfigsDefined(){
+
+		if(isset($this->configs->app->appURL)
+			&& isset($this->configs->router->controllersDirectory)
+			&& isset($this->configs->router->default) 
+			&& is_object($this->configs->router->default)
+			&& isset($this->configs->router->notFound) 
+			&& is_object($this->configs->router->notFound)){
+
+			return true;
+		}
+
+		return false;
 	}
 
 	/**
-	 * Returns an array of URI elements that relate to routing 
-	 * within this app.
-	 */
+	* Returns an array of URI elements that relate to routing 
+	* within this app.
+	*/
 	public function getUri(){
-	    
-	    $rqst = SymfonyRequest::get();
-	    $host = trim(
-			    	$rqst->object->server->get('HTTP_HOST'), 
-			    	'www.'
-			    );
-	    
-	    $fullURI = $rqst->object->server->get('REQUEST_URI');
-	    
-	    if(strpos($this->configs->app->appURL, $host) !== 0){
-	        throw new Exception('The appURL config property does not match the HTTP Host this app is running on.');
-	    }
 
-	    // If this app was not installed in the root directory of
-	    // the domain, the $appURI would carry the path to where it
-	    // is running from.
-	    $appURI = trim(
-			    		substr(
-			    			$this->configs->app->appURL, 
-			    			strlen($host)
-			    		), 
-			    		'/'
-			    	);
-	    
-	    // Get the URI that relates to routing within the app...
-	    // In other words; the URI without the $appURI defined above.
-	    $uri = trim(
-		    		substr(
-		    			trim($fullURI, '/'), 
-		    			strlen($appURI)
-		    		), 
-	    		'/');
-	    
-	    // Get rid of the query string, explode path into an array
-	    // and clean each element before sending to router.
-	    $pathArr = explode('?', $uri);
-	    $arr = explode('/', $pathArr[0]);
+		$rqst = SymfonyRequest::get();
+		$host = trim(
+					$rqst->object->server->get('HTTP_HOST'), 
+					'www.'
+					);
 
-	    if(!is_array($arr)){
-	    	return [];
-	    }
-	    
-	    foreach ($arr as $k => $value) {
-	        preg_match('#([A-Za-z1-9_-])+#', $value, $matches);
-	        $arr[$k] = $matches[0];
-	    }
+		$fullURI = $rqst->object->server->get('REQUEST_URI');
 
-	    return $arr;
+		if(strpos($this->configs->app->appURL, $host) !== 0){
+			throw new Exception('The appURL config property does not match the HTTP Host this app is running on.');
+		}
+
+		// If this app was not installed in the root directory of
+		// the domain, the $appURI would carry the path to where it
+		// is running from.
+		$appURI = trim(
+						substr(
+							$this->configs->app->appURL, 
+							strlen($host)
+						), 
+						'/'
+					);
+
+		// Get the URI that relates to routing within the app...
+		// In other words; the URI without the $appURI defined above.
+		$uri = trim(
+					substr(
+						trim($fullURI, '/'), 
+						strlen($appURI)
+					), 
+					'/'
+				);
+
+		// Get rid of the query string, explode path into an array
+		// and clean each element before sending to router.
+		$pathArr = explode('?', $uri);
+		$arr = explode('/', $pathArr[0]);
+
+		if(!is_array($arr)){
+			return [];
+		}
+
+		foreach ($arr as $k => $value) {
+			preg_match('#([A-Za-z1-9_-])+#', $value, $matches);
+			$arr[$k] = $matches[0];
+		}
+
+		return $arr;
 	}
 
 }
