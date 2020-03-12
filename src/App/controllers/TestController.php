10c154d398b248f5cfa205edb2e5180dd9843d4b
Mustafa Ghayyur <mghayyur@gmail.com>
2020-03-11 23:19:22 -0400

    FilterText seems to be ready to test

--- src/App/controllers/TestController.php
+++ src/App/controllers/TestController.php
@@ -12,7 +12,45 @@ class TestController extends Controller{
 
 		$f = new FilterText();
 
-		$f->process();
+		$text = <<<EOD
+<!DocType html />
+<html>
+	<head>
+		<title>[Dotz Framework] Get Example</title>
+		<link rel="stylesheet" type="text/css" href="http://localhost/WDRouterLibrary/src/App/Views/assets/css/styles.css">
+
+		
+<script type="text/javascript">
+var dotz = {"------------":"Full URL including HTTP protocol.-------","name":"Web Dotz","url":"http:\/\/localhost\/WDRouterLibrary"}
+</script>
+	</head>
+	<body>
+		<div class="page">
+			<a href="http://www.web-dotz.com" target="_blank" class="logo"></a>
+			<div class="content">
+
+
+<h1>Example select queries output:</h1>
+<p>If you are seeing this sentence in the browser; your database and doctrine are setup correctly!</p>
+<p>This is the second query.</p>
+<p>This is the third query.</p>
+	
+
+			</div>
+			<div class="menu">
+				<a href="http://localhost/WDRouterLibrary" class="item">Home</a>
+				<a href="http://localhost/WDRouterLibrary/pages/form" class="item">Form Example</a>
+				<a href="http://localhost/WDRouterLibrary/pages/queries" class="item">MySQL Query example</a>
+				<a href="http://localhost/WDRouterLibrary/get/secure?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3RcL1dEUm91dGVyTGlicmFyeSIsImlhdCI6MTU4Mzg3MTg5MiwiZXhwIjoxNTgzODcyNDkyfQ.cENuJPoh1VA-qCtdqK58OlwuYFqN3g1YAbIqUSfDoMc&index=<script>var t='hello'; document.write(t);</script>" class="item">Get Filtering Example (JWT)</a>
+				<a href="http://localhost/WDRouterLibrary/get?index=<script>var t='hello'; document.write(t);</script>" class="item">Get Filtering Example</a>
+			</div>
+		</div>
+	</body>
+</html>
+<div class="profiler">Time: 0.078s<br/>Memory: 828.584kb</div>
+EOD;
+
+		$f->process($text);
 
 		//$packet = [ 'msg' => 'Developed by Web Dotz' ];
 		//$this->view->load('home', $packet);
--- src/DotzFramework/Modules/Form/FilterText.php
+++ src/DotzFramework/Modules/Form/FilterText.php
@@ -7,6 +7,8 @@ class FilterText {
 
 	public $allowedTags;
 
+	public $removeOnlyTag;
+
 	public function __construct($allowedTags = []){
 		
 		$this->allowedTags = [
@@ -17,7 +19,19 @@ class FilterText {
 			'table',
 			'th',
 			'tr',
-			'td'
+			'td',
+			'br',
+			'h1',
+			'h2',
+			'h3',
+			'h4',
+			'h5',
+			'h6',
+		];
+
+		$this->removeOnlyTag = [
+			'html',
+			'body'
 		];
 
 		array_merge($this->allowedTags, $allowedTags);
@@ -26,70 +40,65 @@ class FilterText {
 
 	public function process($text = null){
 
-		$text = <<<EOD
-<!DocType html />
-<html>
-	<head>
-		<title>[Dotz Framework] Get Example</title>
-		<link rel="stylesheet" type="text/css" href="http://localhost/WDRouterLibrary/src/App/Views/assets/css/styles.css">
-
-		
-<script type="text/javascript">
-var dotz = {"------------":"Full URL including HTTP protocol.-------","name":"Web Dotz","url":"http:\/\/localhost\/WDRouterLibrary"}
-</script>
-	</head>
-	<body>
-		<div class="page">
-			<a href="http://www.web-dotz.com" target="_blank" class="logo"></a>
-			<div class="content">
-
-
-<h1>Example select queries output:</h1>
-<p>If you are seeing this sentence in the browser; your database and doctrine are setup correctly!</p>
-<p>This is the second query.</p>
-<p>This is the third query.</p>
-	
-
-			</div>
-			<div class="menu">
-				<a href="http://localhost/WDRouterLibrary" class="item">Home</a>
-				<a href="http://localhost/WDRouterLibrary/pages/form" class="item">Form Example</a>
-				<a href="http://localhost/WDRouterLibrary/pages/queries" class="item">MySQL Query example</a>
-				<a href="http://localhost/WDRouterLibrary/get/secure?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3RcL1dEUm91dGVyTGlicmFyeSIsImlhdCI6MTU4Mzg3MTg5MiwiZXhwIjoxNTgzODcyNDkyfQ.cENuJPoh1VA-qCtdqK58OlwuYFqN3g1YAbIqUSfDoMc&index=<script>var t='hello'; document.write(t);</script>" class="item">Get Filtering Example (JWT)</a>
-				<a href="http://localhost/WDRouterLibrary/get?index=<script>var t='hello'; document.write(t);</script>" class="item">Get Filtering Example</a>
-			</div>
-		</div>
-	</body>
-</html>
-<div class="profiler">Time: 0.078s<br/>Memory: 828.584kb</div>
-EOD;
-		// "|(<[^>]+>)(.*)</[^>]+>|U"
-		// "|(<[^>]+>)|U"
-		/*preg_match_all("|(<[^>]+>)|U",
-		    $text,
-		    $t, PREG_PATTERN_ORDER
-		);*/
-
 		$arr = explode("\n", $text);
+		$removed = [];
 
+		// loop through each line.
 		foreach ($arr as $i => $line) {
+			
 			$arr[$i] = trim($line);
 
 			preg_match_all("|(<[^>]+>)|U",
-			    $line,
-			    $t, PREG_PATTERN_ORDER
+			    $line, $tags, PREG_PATTERN_ORDER
 			);
 
-			for ($s=0; $s < count($t[0]); $s++) { 
-				preg_match('##', $t[0], $m);
+			// loop through each tag and pick out opening tags only
+			// to further nick-pick
+			for ($s=0; $s < count($tags[0]); $s++) { 
+				
+				preg_match('#<!?([a-zA-Z0-1]+)#', 
+					$tags[0][$s], $m
+				);
+				
+				if(count($m) > 1){
+					
+					if(!in_array(strtolower($m[1]), $this->allowedTags)){
 
+						if(in_array(strtolower($m[1]), $this->removeOnlyTag)){
+							
+							// remove only the opening and closing tags...
+							$text = preg_replace("#</?".$m[1].".*/?>#Us",
+								'', $text, -1, $c 
+							);
+
+						}else{
+
+							// need to remove the whole tag and its contents.
+							$text = preg_replace("#<".$m[1].".*>(.*)</".$m[1].".*>#Us",
+								'', $text, -1, $c 
+							);
+
+							if($c == 0){
+
+								// if that fails, at least remove the opening and closing tags.
+								$text = preg_replace("#</?!?".$m[1].".*/?>#Us",
+									'', $text, -1, $c 
+								);
+							}
+
+						}
+		
+						$removed[strtolower($m[1])] = $c;
+					}
+				}
 
 			}
 		}
 
-		var_dump($t[0], $arr);
+		$text = trim($text);
+
+		var_dump($arr, $removed, $text);
 
 	}
-
 
 }
